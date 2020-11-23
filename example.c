e16fada449c0d55eef98368dac98dbfbeba171ce
Vincent Tavernier <vince.tavernier@gmail.com>
2020-11-23 20:56:09 +0100

    Add comments

--- example.c
+++ example.c
@@ -6,11 +6,11 @@
 struct ThreadPool;
 typedef struct ThreadPool ThreadPool;
 
-extern ThreadPool* new_rayon_pool();
-extern void do_something_with_pool(ThreadPool *pool);
+extern ThreadPool* new_rayon_pool();                  // from crate_a
+extern void do_something_with_pool(ThreadPool *pool); // from_crate_b
 
 int main(int argc, char *argv[]) {
-	ThreadPool *pool = new_rayon_pool();
-	do_something_with_pool(pool);
+	ThreadPool *pool = new_rayon_pool(); // works
+	do_something_with_pool(pool);        // assertion fails
 	return 0;
 }
